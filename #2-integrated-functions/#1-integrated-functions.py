0ef154ee0eb7fff239c4cbef5b6648c62575dbe8
Taras Melnykov <tarik762@ukr.net>
2023-04-28 16:47:36 +0300

    #2-integrated-finctions

---
+++ #2-integrated-functions/#1-integrated-functions.py
@@ -0,0 +1,15 @@
+import sys
+
+#print
+print("Hello Python")
+print('fffd'.capitalize())
+print(print)
+
+#input
+name = input("Input your name") #Forever is 'str' in
+
+print(name)
+
+
+#n = sys.argv;
+#print (n);
---
+++ #2-integrated-functions/practice.py
@@ -0,0 +1,26 @@
+#tests print()
+
+#print (10, 'Taras', True)
+#print(print)
+#print(input)
+#print(dir)
+
+#__xxxx__ - systems attributes (2 underscores)
+#We output list of all build in functions
+#print(dir(__builtins__))
+
+
+#tests input()
+name = input ("Enter your name: ")
+#city = input ("Enter your city: ")
+#phone = input ("Enter your phone: ")
+#print ("name: "+name.upper()+", city: "+city.capitalize()+", phone: "+phone.capitalize())
+#print(dir(name))
+
+'''
+dir(name)
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+'''
+
+print(name)
+print('*'.join(name.split('a')));
