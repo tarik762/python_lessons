eae90d8ad0ab5c339fab90f4fa3ede29583a380e
Taras Melnykov <tarik762@ukr.net>
2023-04-28 13:27:28 +0300

    #1 - main conceptions

---
+++ #1-main-conceptions/#1-data-types.py
@@ -0,0 +1,11 @@
+str = 'Taras'                   #String type (prototype of class 'str')
+bool = True                     #Bool type (prototype of class 'bool')
+int = 10                        #Integer type (prototype of class "int")
+float = 3.34                    #Float type (prototype of class "float")
+list = [1,2,3,'5']              #List type (prototype of class "list")
+dict = {'a':1, 'b':2, 'f4':67}  #Dictionary type (prototype of class "dict")
+tuple = (3,4,5)                 #Tuple type (prototype of class "tuple")
+
+
+print(list[2])
+print(dict["a"])
